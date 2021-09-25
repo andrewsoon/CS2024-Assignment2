d59ebaf4d2340fe72bdb40d243cde71ce50f279f
andrewsoon <andrewsoon.tx@gmail.com>
2021-09-25 21:36:19 +0800

    Fix header, navbar in progress

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,26 @@
 window.onscroll = function () {
-  myFunction();
+  headerFunction();
 };
 
-function myFunction() {
+function headerFunction() {
   if (
-    document.body.scrollTop > 300 ||
-    document.documentElement.scrollTop > 300
+    document.body.scrollTop > 550 ||
+    document.documentElement.scrollTop > 550
   ) {
     document.getElementById("headerID").className = "header-white";
     document.getElementById("header-link").style.color = "rgb(40, 27, 54)";
     document.getElementById("header-link2").style.color = "rgb(40, 27, 54)";
+    document.getElementById("apply-cta").className = "apply-loan2";
   } else {
     document.getElementById("headerID").className = "header-transparent";
     document.getElementById("header-link").style.color = "#ececec";
     document.getElementById("header-link2").style.color = "#ececec";
+    document.getElementById("apply-cta").className = "apply-loan";
   }
 }
+
+// function subpageHeader() {
+//   console.log("haha");
+//   document.getElementById("header-link").style.color = "rgb(40,27,54)";
+//   document.getElementById("header-link2").style.color = "rgb(40,27,54)";
+// }
