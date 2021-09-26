087837da894dd2807fcae46b505f3a1c5fa24b9e
andrewsoon <andrewsoon.tx@gmail.com>
2021-09-26 15:44:20 +0800

    Fix navbar and header

--- javascript/index.js
+++ javascript/index.js
@@ -8,19 +8,13 @@ function headerFunction() {
     document.documentElement.scrollTop > 550
   ) {
     document.getElementById("headerID").className = "header-white";
-    document.getElementById("header-link").style.color = "rgb(40, 27, 54)";
-    document.getElementById("header-link2").style.color = "rgb(40, 27, 54)";
-    document.getElementById("apply-cta").className = "apply-loan2";
+    document.getElementById("header-link").className = "header-nav-whiteBG";
+    document.getElementById("header-link2").className = "header-nav-whiteBG";
+    document.getElementById("apply-cta").className = "apply-loan-whiteBG";
   } else {
     document.getElementById("headerID").className = "header-transparent";
-    document.getElementById("header-link").style.color = "#ececec";
-    document.getElementById("header-link2").style.color = "#ececec";
+    document.getElementById("header-link").className = "header-nav";
+    document.getElementById("header-link2").className = "header-nav";
     document.getElementById("apply-cta").className = "apply-loan";
   }
 }
-
-// function subpageHeader() {
-//   console.log("haha");
-//   document.getElementById("header-link").style.color = "rgb(40,27,54)";
-//   document.getElementById("header-link2").style.color = "rgb(40,27,54)";
-// }
